010af97b6656b88f1d60c418958702dff2383144
aktherhosen <dev.aktherhosen@gmail.com>
2025-10-01 12:46:17 +0600

    feat: register route, interface, schema and model added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+import cors from "cors";
 import express, { type Request, type Response } from "express";
+import { UserRoutes } from "./app/modules/user/user.route";
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/user", UserRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to Tour Management",
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { User } from "./user.model";
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = User.create({
+      name,
+      email,
+    });
+    res.status(httpStatus.CREATED).json({
+      message: "User created successfully.",
+      user,
+    });
+  } catch (err: any0) {
+    console.log(err);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: `Something went wrong ! ${err.message}`,
+      err,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
