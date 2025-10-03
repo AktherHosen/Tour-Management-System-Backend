238445ace9c6cd81e0d6e4a93ebde95f2a623c92
aktherhosen <dev.aktherhosen@gmail.com>
2025-10-03 20:59:49 +0600

    feat: added not found route, restructure try catch with catch async

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
-import express, { type Request, type Response } from "express";
+import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandlers";
+import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 const app = express();
 
@@ -17,5 +18,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,47 @@
-/* eslint-disable no-console */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
 import { UserServices } from "./user.service";
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const user = await UserServices.createUser(req.body);
+//     res.status(httpStatus.CREATED).json({
+//       message: "User created successfully.",
+//       user,
+//     });
+//   } catch (err: any) {
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+const createUser = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
     res.status(httpStatus.CREATED).json({
       message: "User created successfully.",
       user,
     });
-  } catch (err: any) {
-    console.log(err);
-    next(err);
   }
-};
+);
+
+const getAllUsers = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All users retrieved successfully",
+      data: users,
+    });
+  }
+);
 
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
 
 /** __FLOW__
