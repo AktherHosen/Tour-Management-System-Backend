4d191ee9a3610d13b9c366f2627352fd760ea1be
aktherhosen <dev.aktherhosen@gmail.com>
2025-10-03 21:44:56 +0600

    feat: add sendResponse helper function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { UserServices } from "./user.service";
 
 // const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -20,9 +21,11 @@ const createUser = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({
-      message: "User created successfully.",
-      user,
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
+      message: "User created successfully",
+      success: true,
+      data: user,
     });
   }
 );
@@ -30,11 +33,14 @@ const createUser = catchAsync(
 const getAllUsers = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserServices.getAllUsers();
-    res.status(httpStatus.OK).json({
-      success: true,
+    const results = await UserServices.getAllUsers();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       message: "All users retrieved successfully",
-      data: users,
+      success: true,
+      data: results.data,
+      meta: results.meta
     });
   }
 );
