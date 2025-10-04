761f669af4b81e91daf95ab31c992a3bd205af4f
aktherhosen <dev.aktherhosen@gmail.com>
2025-10-04 09:04:51 +0600

    feat: add zod  validations

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { AnyZodObject } from "zod";
 import { UserControllers } from "./user.controller";
-
+import { createUserZodSchema } from "./user.validations";
 const router = Router();
 
-router.post("/register", UserControllers.createUser);
+const validateRequest =
+  (zodSchema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.body = await zodSchema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
 router.get("/all-users", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
